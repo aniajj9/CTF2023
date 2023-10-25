2be0c157132284a596df11a27c590c80b92493b0
aniajj9 <56159175+aniajj9@users.noreply.github.com>
2023-10-25 20:43:39 +0200

    Update docker-compose-recurse.py

--- docker-compose-recurse.py
+++ docker-compose-recurse.py
@@ -15,6 +15,9 @@ def run_docker_compose(file_path):
     subprocess.run(['docker-compose', '-f', file_path, 'up', '-d', '--build'])
 
 def main(root_directory):
+    # Set execute permissions for all .sh files
+    subprocess.run(['find', root_directory, '-type', 'f', '-name', '*.sh', '-exec', 'chmod', '+x', '{}', ';'])
+
     for docker_compose_file in find_docker_compose_files(root_directory):
         print(f"Running Docker Compose for: {docker_compose_file}")
         run_docker_compose(docker_compose_file)
